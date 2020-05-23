a323bf01a6eac120d98e816b44b05ecda1042269
James Lynott <41302574+jcelynott@users.noreply.github.com>
2020-05-23 15:05:55 -0400

    Just a little stubby version of the list

---
+++ src/components/TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CheckboxList extends Component{
+    
+    render(){
+        return(
+            <div>
+                A very fun list! :O
+            </div>
+        )
+    }
+}
