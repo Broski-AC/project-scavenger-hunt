ad8bbfc7deb61803d3d28876882f27fa199279c7
Broski-AC <acbroski@oakland.edu>
2020-05-24 04:39:36 -0400

    Should have fixed the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import TaskList from './components/TaskList';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import UserRegistration from "./components/userRegistration.component";
+import UserLogin from "./components/userLogin.component";
 
 function App() {
 
@@ -9,11 +13,16 @@ function App() {
   const [logged, setLogged] = useState(false);
 
   return (
-    <div className="container">
-        Scavenger
-        <br/>
-        <TaskList/>
-    </div>
+    <Router>
+      <div className="container">
+          <Navbar />
+          <Route path="/api/users" component={UserRegistration} />
+          <Route path="/QuestEntry" component={TaskList} />
+          <Route path="/api/auth/login" component={UserLogin} />
+         
+          {/* <TaskList/> */}
+      </div>
+    </Router>
   );
 }
 
